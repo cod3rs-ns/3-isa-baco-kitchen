6aee25b16f95d58ac7cfdb51e856ef832978998a
Stefan Ristanović <st.keky@gmail.com>
2016-04-07 20:14:15 +0200

    JavaBeans and example of RestController, Service and Repository.

---
+++ src/main/java/com/bacovakuhinja/model/User.java
@@ -0,0 +1,86 @@
+package com.bacovakuhinja.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * This is base class for all types of Users. It has all shared properties and all of them are based inside one
+ * database table called 'users'.
+ */
+
+@Entity
+@Table(name="users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "u_id")
+    private Integer userId;
+
+    @Column(name = "u_fname")
+    private String firstName;
+
+    @Column(name = "u_lname")
+    private String lastName;
+
+    @Column(name = "u_email")
+    private String email;
+
+    @Column(name = "u_image")
+    private String image;
+
+    @Column(name = "u_password")
+    private String password;
+
+    public User(){
+
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/bacovakuhinja/service/RestaurantProviderService.java
@@ -0,0 +1,14 @@
+package com.bacovakuhinja.service;
+
+import com.bacovakuhinja.model.RestaurantProvider;
+
+import java.util.Collection;
+
+/**
+ * Created by St_Keky on 7.4.2016..
+ */
+public interface RestaurantProviderService {
+
+    public Collection<RestaurantProvider> findAll();
+
+}
