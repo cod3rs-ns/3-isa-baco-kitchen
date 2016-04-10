cabac8b050e06f4a121cdfdabf853e841bffaee1
Stefan Ristanović <st.keky@gmail.com>
2016-04-10 21:25:43 +0200

    DB schema updated.

--- src/main/data/isamrs_db_schema.sql
+++ src/main/data/isamrs_db_schema.sql
@@ -27,6 +27,7 @@ CREATE TABLE IF NOT EXISTS `isa_mrs_project`.`users` (
   `u_email` VARCHAR(45) NOT NULL,
   `u_image` VARCHAR(100) NULL,
   `u_password` VARCHAR(45) NOT NULL,
+  `u_type` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`u_id`))
 ENGINE = InnoDB;
 
@@ -96,7 +97,14 @@ CREATE TABLE IF NOT EXISTS `isa_mrs_project`.`restaurants` (
   `r_type` VARCHAR(45) NULL,
   `r_time_start` INT NULL,
   `r_time_end` INT NULL,
-  PRIMARY KEY (`r_id`))
+  `r_sm_id` INT NOT NULL,
+  PRIMARY KEY (`r_id`),
+  INDEX `fk_restaurants_sys_managers1_idx` (`r_sm_id` ASC),
+  CONSTRAINT `fk_restaurants_sys_managers1`
+    FOREIGN KEY (`r_sm_id`)
+    REFERENCES `isa_mrs_project`.`sys_managers` (`sm_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -136,6 +144,54 @@ CREATE TABLE IF NOT EXISTS `isa_mrs_project`.`restaurant_providers` (
   CONSTRAINT `rp_fid`
     FOREIGN KEY (`rp_id`)
     REFERENCES `isa_mrs_project`.`users` (`u_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `isa_mrs_project`.`bartenders`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `isa_mrs_project`.`bartenders` ;
+
+CREATE TABLE IF NOT EXISTS `isa_mrs_project`.`bartenders` (
+  `b_id` INT NOT NULL,
+  PRIMARY KEY (`b_id`),
+  CONSTRAINT `b_fid`
+    FOREIGN KEY (`b_id`)
+    REFERENCES `isa_mrs_project`.`employees` (`e_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `isa_mrs_project`.`cooks`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `isa_mrs_project`.`cooks` ;
+
+CREATE TABLE IF NOT EXISTS `isa_mrs_project`.`cooks` (
+  `c_id` INT NOT NULL,
+  PRIMARY KEY (`c_id`),
+  CONSTRAINT `c_fid`
+    FOREIGN KEY (`c_id`)
+    REFERENCES `isa_mrs_project`.`employees` (`e_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `isa_mrs_project`.`waiters`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `isa_mrs_project`.`waiters` ;
+
+CREATE TABLE IF NOT EXISTS `isa_mrs_project`.`waiters` (
+  `w_id` INT NOT NULL,
+  PRIMARY KEY (`w_id`),
+  CONSTRAINT `w_fid`
+    FOREIGN KEY (`w_id`)
+    REFERENCES `isa_mrs_project`.`employees` (`e_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
--- src/main/data/isamrs_test_data.sql
+++ src/main/data/isamrs_test_data.sql
@@ -1,18 +1,31 @@
-INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`) VALUES ('1', 'Sergio', 'Ramos', 'sr4@real.com', 'image.png', 'ramos');
-INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`) VALUES ('2', 'Cristiano', 'Ronaldo', 'cr7@real.com', 'image.png', 'cristiano');
-INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`) VALUES ('3', 'Jamie', 'Vardy', 'jv@leilcester.com', 'image.jpg', 'party');
-INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`) VALUES ('4', 'Riyad', 'Mahrez', 'rm@leicester.com', 'image.gif', 'mahry');
-INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`) VALUES ('5', 'David', 'Beckham', 'becks@legend.com', 'image.png', 'retired');
-INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`) VALUES ('6', 'Zinedine', 'Zidane', 'zizu@manager.com', 'image.png', 'headshot');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('1', 'Sergio', 'Ramos', 'sr4@real.com', 'image.png', 'ramos', 'guest');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('2', 'Cristiano', 'Ronaldo', 'cr7@real.com', 'image.png', 'cristiano', 'guest');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('3', 'Jamie', 'Vardy', 'jv@leilcester.com', 'image.jpg', 'party', 'restaurant_manager');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('4', 'Riyad', 'Mahrez', 'rm@leicester.com', 'image.gif', 'mahry', 'restaurant_provider');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('5', 'David', 'Beckham', 'becks@legend.com', 'image.png', 'retired', 'restaurant_provider');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('6', 'Zinedine', 'Zidane', 'zizu@manager.com', 'image.png', 'headshot', 'system_manager');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('7', 'Hugo', 'Vieira', 'hugo@redstar.rs', 'image.png', 'redstar1', 'cook');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('8', 'Damien', 'le Tallec', 'tallec@redstar.rs', 'image.png', 'redstar2', 'bartender');
+INSERT INTO `isa_mrs_project`.`users` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_image`, `u_password`, `u_type`) VALUES ('9', 'Michel', 'Donald', 'dm@redstar.rs', 'image.png', 'redstar3', 'waiter');
 
-INSERT INTO `isa_mrs_project`.`restaurants` (`r_id`, `r_name`, `r_info`, `r_type`, `r_time_start`, `r_time_end`) VALUES ('1', 'Macchiato', 'Macchiato Liman vama na usluzi. Najlepši pogled na Limanski park u gradu xD.', 'Custom', '6', '23');
-INSERT INTO `isa_mrs_project`.`restaurants` (`r_id`, `r_name`, `r_info`, `r_type`, `r_time_start`, `r_time_end`) VALUES ('2', 'Orfei', 'Restoran Orfei vama na usluzi.', 'Custom', '7', '21');
-INSERT INTO `isa_mrs_project`.`restaurants` (`r_id`, `r_name`, `r_info`, `r_type`, `r_time_start`, `r_time_end`) VALUES ('3', 'Mašinac', 'Studenti na prvom mestu.', 'Studentski', '6', '18');
+INSERT INTO `isa_mrs_project`.`sys_managers` (`sm_id`, `sm_info`) VALUES ('6', 'Zidane biography.');
+
+INSERT INTO `isa_mrs_project`.`restaurants` (`r_id`, `r_name`, `r_info`, `r_type`, `r_time_start`, `r_time_end`,  `r_sm_id`) VALUES ('1', 'Macchiato', 'Macchiato Liman vama na usluzi. Najlepši pogled na Limanski park u gradu xD.', 'Custom', '6', '23', '6');
+INSERT INTO `isa_mrs_project`.`restaurants` (`r_id`, `r_name`, `r_info`, `r_type`, `r_time_start`, `r_time_end`,  `r_sm_id`) VALUES ('2', 'Orfei', 'Restoran Orfei vama na usluzi.', 'Custom', '7', '21', '6');
+INSERT INTO `isa_mrs_project`.`restaurants` (`r_id`, `r_name`, `r_info`, `r_type`, `r_time_start`, `r_time_end`,  `r_sm_id`) VALUES ('3', 'Mašinac', 'Studenti na prvom mestu.', 'Studentski', '6', '18', '6');
 
 INSERT INTO `isa_mrs_project`.`guests` (`g_id`, `g_info`) VALUES ('1', 'Ramos biography.');
 INSERT INTO `isa_mrs_project`.`guests` (`g_id`, `g_info`) VALUES ('2', 'Ronaldo biography.');
 INSERT INTO `isa_mrs_project`.`restaurant_managers` (`rm_id`, `rm_info`, `rm_restaurant_id`) VALUES ('3', 'Vardy biography.', '2');
 INSERT INTO `isa_mrs_project`.`restaurant_providers` (`rp_id`, `rp_info`) VALUES ('4', 'Mahrez biography.');
 INSERT INTO `isa_mrs_project`.`restaurant_providers` (`rp_id`, `rp_info`) VALUES ('5', 'Becks biography.');
-INSERT INTO `isa_mrs_project`.`sys_managers` (`sm_id`, `sm_info`) VALUES ('6', 'Zidane biography.');
+
+
+INSERT INTO `isa_mrs_project`.`employees` (`e_id`, `e_bday`, `e_dress_size`, `e_shoes_size`) VALUES ('7', '1988-06-15', 'L', '42b');
+INSERT INTO `isa_mrs_project`.`employees` (`e_id`, `e_bday`, `e_dress_size`, `e_shoes_size`) VALUES ('8', '1988-07-15', 'XXL', '43b');
+INSERT INTO `isa_mrs_project`.`employees` (`e_id`, `e_bday`, `e_dress_size`, `e_shoes_size`) VALUES ('9', '1988-08-15', 'M', '44b');
+
+INSERT INTO `isa_mrs_project`.`cooks` (`c_id`) VALUES ('7');
+INSERT INTO `isa_mrs_project`.`bartenders` (`b_id`) VALUES ('8');
+INSERT INTO `isa_mrs_project`.`waiters` (`w_id`) VALUES ('9');
 
