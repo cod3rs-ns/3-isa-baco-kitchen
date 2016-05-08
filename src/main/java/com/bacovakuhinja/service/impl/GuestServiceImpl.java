a361c384b31fb70a6e51a9693118b7361f046ce6
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-05-08 23:29:57 +0200

    Users search added.

--- src/main/java/com/bacovakuhinja/service/impl/GuestServiceImpl.java
+++ src/main/java/com/bacovakuhinja/service/impl/GuestServiceImpl.java
@@ -1,11 +1,13 @@
 package com.bacovakuhinja.service.impl;
 
 import com.bacovakuhinja.model.Guest;
+import com.bacovakuhinja.model.User;
 import com.bacovakuhinja.repository.GuestRepository;
 import com.bacovakuhinja.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Service
@@ -27,5 +29,18 @@ public class GuestServiceImpl implements GuestService {
     @Override
     public Guest getGuest(Integer id) {
         return guestRepository.findOne(id);
+    }
+
+    @Override
+    public Collection<User> getUsers(String query) {
+        Collection<User> users = new ArrayList<User>();
+        for (Guest guest : guestRepository.findAll()) {
+            if (guest.getFirstName().toLowerCase().contains(query) || guest.getLastName().toLowerCase().contains(query)) {
+                users.add(guest);
+                if (users.size() >= 5) break;
+            }
+        }
+
+        return users;
     }
 }
