a3835296f3351134067573aea90721cf051d3af7
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-05-10 00:13:45 +0200

    FIXED: Rating on Home page isn't clickable now.

---
+++ src/main/resources/static/js/directives/rating-no-click.directive.js
@@ -0,0 +1,38 @@
+angular.module('isa-mrs-project').directive('starRatingNoClick',
+function() {
+return {
+restrict : 'A',
+template : '<ul class="rating"><li ng-repeat="star in stars" ng-class="star"><i class="material-icons">favorite</i></li></ul>',
+scope : {
+ ratingValue : '=',
+ max : '=',
+ onRatingSelected : '&'
+},
+link : function(scope, elem, attrs) {
+ var updateStars = function() {
+  scope.stars = [];
+  for ( var i = 0; i < scope.max; i++) {
+   scope.stars.push({
+    filled : i < scope.ratingValue
+   });
+  }
+ };
+ 
+ scope.toggle = function(index) {
+  scope.ratingValue = index + 1;
+  scope.onRatingSelected({
+   rating : index + 1
+  });
+ };
+ 
+ scope.$watch('ratingValue',
+  function(oldVal, newVal) {
+   if (newVal) {
+    updateStars();
+   }
+  }
+ );
+}
+};
+}
+);
