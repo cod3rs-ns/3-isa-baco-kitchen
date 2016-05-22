66301ad7b77212c13e069614ed0c409a59b4db0d
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-05-22 18:01:33 +0200

    Needed classes for ReservationTable added.

---
+++ src/main/java/com/bacovakuhinja/repository/ReservationTableRepository.java
@@ -0,0 +1,7 @@
+package com.bacovakuhinja.repository;
+
+import com.bacovakuhinja.model.ReservationTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationTableRepository extends JpaRepository<ReservationTable, Integer> {
+}
---
+++ src/main/java/com/bacovakuhinja/service/ReservationTableService.java
@@ -0,0 +1,12 @@
+package com.bacovakuhinja.service;
+
+import com.bacovakuhinja.model.ReservationTable;
+
+import java.util.Collection;
+
+public interface ReservationTableService {
+
+    public Collection<ReservationTable> findAll();
+
+    public Collection<ReservationTable> findAllByReservationId(Integer reservationId);
+}
